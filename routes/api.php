9452dcc0b3a4c28cdec4abb6f9d16384e09322f7
Cowio <Kasper.gorm@gmail.com>
2025-11-27 09:43:12 +0100

    Remove test model and Slack route
    Deleted the unused test model and removed the Slack message route from the API routes file to clean up unused code.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,4 @@ use App\Http\Controllers\SlackController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExceptionController;
 
-Route::post('/slack/message', [SlackController::class, 'notify']);
-
 Route::post('/exception', [ExceptionController::class, 'report']);
