c53566eea5be24d41bd4824a57c26719bbe04f47
CLNissen <93129177+CLNissen@users.noreply.github.com>
2026-01-14 11:00:07 +0100

    Update ExceptionController.php

--- app/Http/Controllers/ExceptionController.php
+++ app/Http/Controllers/ExceptionController.php
@@ -23,17 +23,5 @@ class ExceptionController extends Controller
 
         // Immediate response to the client
         return response()->json(['status' => 'Exception reported successfully'], 200);
-
-//        $slackController = new SlackController();
-//
-//        // Custom logic to log exception to database or external service can be added here
-//        $exception = CaughtException::fromRequest($validated);
-//
-//
-//        $exception->category = "internal"; // You can categorize exceptions if needed
-//        $exception->hash = md5($exception->message . $exception->file . $exception->line);
-//        $exception->save();
-//
-//        $result = $slackController->notify($exception);
     }
 }
